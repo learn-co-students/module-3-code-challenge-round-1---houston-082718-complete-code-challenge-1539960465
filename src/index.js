ad165eb9c8388c2a3d97333b095960a36c2001d5
github username <github email address>
2018-10-19 11:45:16 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+let imageId = 1221; //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
+const commentsURL = `https://randopic.herokuapp.com/comments/`;
 
-  let imageId = 1 //Enter the id from the fetched image here
+let image = document.querySelector("img");
+let imageName = document.getElementById("name");
+let likesSpan = document.getElementById("likes");
+let ul = document.querySelector("ul");
+const form = document.querySelector("form");
+fetchAndDisplayData();
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+function displayData(response) {
+  const imageData = response;
+  image.src = `${imageData.url}`;
+  image.id = `${imageData.id}`;
+  imageName.innerText = `${imageData.name}`;
+  likesSpan.innerText = `${imageData.like_count}`;
+  imageData.comments.forEach(comment => {
+    ul.innerHTML += `<li>${comment.content}</li>`;
+  });
+}
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+function fetchAndDisplayData() {
+  fetch(imageURL)
+    .then(response => response.json())
+    .then(function(response) {
+      console.log(response);
+      displayData(response);
+    });
+}
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+function increaseLikes() {
+  if (event.target.className === "like_button") {
+    // why does this also need to be declared in here when it's already a global variable???
+    let likesSpan = document.getElementById("likes"); //???
+    // why does this also need to be declared in here when it's already a global variable???
+    likesSpan.innerText = parseInt(likesSpan.innerText) + 1;
 
-})
+    fetch(`https://randopic.herokuapp.com/likes`, {
+      method: "POST",
+      body: JSON.stringify({ image_id: 1221 }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(function() {
+      fetch(imageURL).then(response => response.json());
+    });
+  }
+}
+
+document.addEventListener("click", increaseLikes);
